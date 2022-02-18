702e9524b8508aa82d6f944aa3dbab12d48c4fd5
Filipe Teixeira <shuantsu@gmail.com>
2022-02-18 16:30:15 -0300

    feat: compile_exe.bat uses pyinstaller

--- compile_exe.bat
+++ compile_exe.bat
@@ -1,2 +1,2 @@
-python -m nuitka --enable-plugin=pyqt5 --windows-disable-console --windows-icon-from-ico=icon.ico --follow-imports src\srt_extractor.py
+pyinstaller.exe --onefile --windowed --icon=icon.ico src\srt_extractor.py
 pause
